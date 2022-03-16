58b2fb093667a825a2527f5bb31c2c46813d4371
Julian <juliancorrea93@gmail.com>
2022-03-15 23:36:56 -0400

    Explore std::hash as a temporary fix for the Linker issue if unable to resolve

--- DataStructPractice/HashTable.cpp
+++ DataStructPractice/HashTable.cpp
@@ -1,43 +1,87 @@
 #include <\Users\Julian\Documents\Visual Studio 2015\Projects\DataStructPractice\DataStructPractice\HashTable.h>
 
-void HashTable::add(std::string key, std::string value)
+/*template<typename T>
+std::string HashTable<T>::testHash(std::string key)
 {
-	if (get(key) == "") {
-		//RELOOK AT APPROACH TO KEY VALUE
+	std::cout << hash(key) << std::endl << std::flush;
+}
+
+template<typename T>
+std::string HashTable<T>::hash(std::string key)
+{
+	std::vector<bitset<8>> bit_vector;
+	for (char const &c : key) {
+		bit_vector.push_back(hash(c));
 	}
-	else {
-		key_value_pairs.push_back(KeyValue(hash(key), value));
-	}
+
+	//append the 1 to the binary
+	//append 0's until binary value % 512 == 0
+	return std::string();
 }
 
-void HashTable::remove(std::string key, std::string value)
+template<typename T>
+std::bitset<8> HashTable<T>::to_binary(char const & c)
 {
+	return std::bitset<8>();
 }
+*/
 
-std::string HashTable::get(std::string key)
+template<typename T>
+std::string HashTable<T>::hash(std::string key)
 {
-	for (KeyValue pair : key_value_pairs) {
-		if (pair.getKey() == hash(key)) {
-			return key;
-		}
-	}
-	return "";
+	CryptoPP::SHA256 _hash;
+	unsigned char digest[CryptoPP::SHA256::DIGESTSIZE];
+	_hash.CalculateDigest(digest, (unsigned char*)key.c_str(), key.length());
+	CryptoPP::HexEncoder encoder;
+	std::string output;
+	encoder.Attach(new CryptoPP::StringSink(output));
+	encoder.Put(digest, sizeof(digest));
+	encoder.MessageEnd();
+	return output;
 }
 
-void HashTable::print()
-{
-}
-
-std::string HashTable::testHash(std::string key)
+template<typename T>
+std::string HashTable<T>::testHash(std::string key)
 {
 	return hash(key);
 }
 
-std::string HashTable::hash(std::string key)
+
+template<typename T>
+void HashTable<T>::add(std::string key, std::string value)
 {
-	return std::string();
+	std::string hashed_key = hash(key);
+	
+	
+}
+
+template<typename T>
+bool HashTable<T>::is_full()
+{
+	int items = 0;
+	for (int i = 0; i < (sizeof(table) / sizeof(*table)); i++) {
+		if (table[i] == nullptr) {
+			items += 1;
+		}
+	}
+
+	return items > ((sizeof(table) / sizeof(*table)) / 2)
+	return 0;
+}
+
+template<typename T>
+void HashTable<T>::double_size()
+{
+	T doubled = new T[(sizeof(table) / sizeof(*table)) * 2];
+	for (int i = 0; i < (sizeof(table) / sizeof(*table)); i++) {
+		doubled[i] = table[i];
+	}
+	delete[] table;
+
+	table = doubled;
 }
 
 int main() {
-	HashTable ht;
+	HashTable<int> ht;
+	std::cout << ht.testHash("Fuck");
 }
--- DataStructPractice/HashTable.h
+++ DataStructPractice/HashTable.h
@@ -1,15 +1,29 @@
 #pragma once
 #include <vector>
+#include <bitset>
 #include <\Users\Julian\Documents\Visual Studio 2015\Projects\DataStructPractice\DataStructPractice\KeyValue.h>
+#include "..\cryptopp850\sha.h"
+#include "..\cryptopp850\hex.h"
+#include <iostream>
 
+
+template <typename T> 
 class HashTable {
 public:
+	HashTable() :
+		table(new T[4])
+	{}
 	void add(std::string key, std::string value);
 	void remove(std::string key, std::string value);
 	std::string get(std::string key);
 	void print();
 	std::string testHash(std::string key);
+	
 private:
 	std::string hash(std::string key);
-	std::vector<KeyValue> key_value_pairs;
+	//std::bitset<8> to_binary(char const &c);
+	bool is_full();
+	void double_size();
+	//std::vector<KeyValue> key_value_pairs;
+	T *table;
 };
